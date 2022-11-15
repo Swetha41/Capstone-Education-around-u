0edb41c3077f8f41441f6b2bf4a1c2af6aa89a10
Swetha41 <swethagottimukkala@gmail.com>
2022-11-15 20:00:45 +0530

    new changes

--- server/validator/index.js
+++ server/validator/index.js
@@ -14,6 +14,7 @@ exports.userSignupValidator = (req, res, next) => {
         .matches(/\d/)
         .withMessage("Password must contain a number");
     const errors = req.validationErrors();
+    console.log('error')
     if (errors) {
         const firstError = errors.map(error => error.msg)[0];
         return res.status(400).json({ error: firstError });
